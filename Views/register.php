482972ac71bd97f849d5a7194f77b1f39425f86c
Lahmar22 <lahmarzakariae22@gmail.com>
2025-12-31 20:10:09 +0100

    feat : update page register and desing

--- Views/register.php
+++ Views/register.php
@@ -52,7 +52,7 @@
         </div>
 
         <div class="glass rounded-2xl p-8">
-            <form action="#" method="POST" class="space-y-5">
+            <form action="../Controllers/Inscription.php" method="POST" class="space-y-5">
                 
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div class="space-y-1">
